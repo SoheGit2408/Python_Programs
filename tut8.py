37c4b1a48e4bff020f64210905f519995f5e864a
SoheGit2408 <sohelshaikh24082000@gmail.com>
2022-11-15 05:38:26 +0000

    turtle module demo program

---
+++ tut8.py
@@ -0,0 +1,23 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily
+"""
+import turtle
+
+ninja = turtle.Turtle()
+ninja.speed(10)
+
+for i in range(180):
+    ninja.forward(100)
+    ninja.right(30)
+    ninja.forward(20)
+    ninja.left(60)
+    ninja.forward(50)
+    ninja.right(30)
+    ninja.penup()
+    ninja.setposition(0, 0)
+    ninja.pendown()
+    ninja.right(2)
+
+ninja.down()
